373c309b8b231ef4c3ca274039314bc6fa4bebfc
Josh Bosley <broskisworld@gmail.com>
2023-10-28 17:01:33 -0600

    Window loads with black bg

--- LiquidSim/main.cpp
+++ LiquidSim/main.cpp
@@ -1,14 +1,34 @@
-//
-//  main.cpp
-//  LiquidSim
-//
-//  Created by Josh Bosley on 10/28/23.
-//
-
 #include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+ 
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+ 
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+ 
+        // Clear screen
+        window.clear();
+        
+        // Update the window
+        window.display();
+    }
+    
+    cout << "Window closed. Exiting...\n";
+ 
+    return EXIT_SUCCESS;
 }
